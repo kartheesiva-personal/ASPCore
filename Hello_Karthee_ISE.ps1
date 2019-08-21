fd77ee49c0dba9ad2dc3c6f63f24961491463f65
kartheesiva-personal <54238010+kartheesiva-personal@users.noreply.github.com>
2019-08-21 17:31:20 +0800

    latest modification

---
+++ Hello_Karthee_ISE.ps1
@@ -0,0 +1,15 @@
+﻿Set-Variable -Name "RootUrl" -value "C:\Karthee\Learning Materials\Powershell\Power_shell_sample\"
+get-childitem -path $RootUrl"SFTWebBackup\*" | Remove-Item -Recurse
+Copy-Item -path $RootUrl"SFTWeb\*" -Destination $RootUrl"SFTWebBackup\" -Recurse
+Remove-Item -path $RootUrl"SFTWeb\*" -Recurse
+if(Test-Path $RootUrl"Publish\appsettings.json")
+{
+    Remove-Item -path $RootUrl"Publish\appsettings.json"
+}
+if(Test-Path $RootUrl"Publish\NLog.xml")
+{
+Remove-Item -path $RootUrl"Publish\NLog.xml"
+}
+Copy-Item -path $RootUrl"Publish\*" -Destination $RootUrl"SFTWeb\" -Recurse
+Copy-Item -path $RootUrl"SFTWebBackup\appsettings.json" -Destination $RootUrl"SFTWeb\"
+Copy-Item -path $RootUrl"SFTWebBackup\NLog.xml" -Destination $RootUrl"SFTWeb\"
